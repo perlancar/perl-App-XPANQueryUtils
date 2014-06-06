842de65446613a8a80bc3558a82be622db348205
Steven Haryanto (on PC, Jakarta) <stevenharyanto@gmail.com>
2014-06-06 12:58:17 +0700

    Initial commit

---
+++ lib/App/XPANQueryUtils.pm
@@ -0,0 +1,26 @@
+package App::XPANQueryUtils;
+
+use 5.010001;
+use strict;
+use warnings;
+
+# VERSION
+# DATE
+
+1;
+# ABSTRACT: XPAN::Query command-line utilities
+
+=head1 SYNOPSIS
+
+This distribution provides the following command-line utilities:
+
+ list-xpan-authors
+ list-xpan-packages
+ list-xpan-dists
+
+
+=head1 SEE ALSO
+
+L<XPAN::Query>
+
+=cut
